f5b7b7fe076f2d083e3f435f6e9426547e37d13f
Oorakathan <oorakathanr@gmail.com>
2026-04-21 10:02:49 +0530

    final answer

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,23 @@
+import axios from 'axios'
+
 const API = 'https://t4e-testserver.onrender.com/api'
 const ID = 'E0323035'
 const SET = 'setA'
 const PASS = '635856'
 
 const getToken = async () => {
-  const res = await fetch(`${API}/public/token`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ studentId: ID, set: SET, password: PASS })
+  const res = await axios.post(`${API}/public/token`, {
+    studentId: ID,
+    set: SET,
+    password: PASS
   })
-  const data = await res.json()
-  return data.token
+  return res.data.token
 }
 
 export const fetchOrders = async () => {
   const token = await getToken()
-  const res = await fetch(`${API}/private/setA`, { headers: { 'Authorization': `Bearer ${token}` } })
-  const data = await res.json()
-  return data.data.orders || []
+  const res = await axios.get(`${API}/private/setA`, {
+    headers: { 'Authorization': `Bearer ${token}` }
+  })
+  return res.data.data.orders || []
 }
